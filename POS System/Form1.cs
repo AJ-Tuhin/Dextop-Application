2c723e98c00fe8edc1a27373ac3a5c70cc6c171c
Md. Al Jobaed <tuhincse001@gmail.com>
2018-03-06 01:07:12 +0600

    Add files via upload

---
+++ POS System/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BDJBatch18.POS.Presentation;
+
+namespace BDJBatch18.POS
+{
+    public partial class Form1 : Form
+    {
+
+        Presentation.frmProductImage productImage = new Presentation.frmProductImage();
+        frmBrand brand = new frmBrand();
+        frmCategory category = new frmCategory();
+        frmCity city = new frmCity();
+        frmLedger ledger = new frmLedger();
+        frmProduct product = new frmProduct();
+        frmProductPrice productPrice = new frmProductPrice();
+        frmPurchase purchase = new frmPurchase();
+        frmTransaction transaction = new frmTransaction();
+        frmUnit unit = new frmUnit();
+        frmEmployee employee = new frmEmployee();
+        //frmPurchaseDetails purchaseDetails = new frmPurchaseDetails();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (productImage.IsDisposed)
+                productImage = new Presentation.frmProductImage();
+            productImage.MdiParent = this;
+            productImage.Show();
+            productImage.BringToFront();
+        }
+
+        private void brandToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (brand.IsDisposed)
+                brand = new frmBrand();
+            brand.MdiParent = this;
+            brand.Show();
+            brand.BringToFront();
+        }
+
+        private void categoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (category.IsDisposed)
+                category = new frmCategory();
+            category.MdiParent = this;
+            category.Show();
+            category.BringToFront();
+        }
+
+        private void cityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (city.IsDisposed)
+                city = new frmCity();
+            city.MdiParent = this;
+            city.Show();
+            city.BringToFront();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (ledger.IsDisposed)
+                ledger = new frmLedger();
+            ledger.MdiParent = this;
+            ledger.Show();
+            ledger.BringToFront();
+        }
+
+        private void productToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (product.IsDisposed)
+                product = new frmProduct();
+            product.MdiParent = this;
+            product.Show();
+            product.BringToFront();
+        }
+
+        private void productPriceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (productPrice.IsDisposed)
+                productPrice = new frmProductPrice();
+            productPrice.MdiParent = this;
+            productPrice.Show();
+            productPrice.BringToFront();
+        }
+
+        private void purchaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (purchase.IsDisposed)
+                purchase = new frmPurchase();
+            purchase.MdiParent = this;
+            purchase.Show();
+            purchase.BringToFront();
+        }
+
+        private void transactionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (transaction.IsDisposed)
+                transaction = new frmTransaction();
+            transaction.MdiParent = this;
+            transaction.Show();
+            transaction.BringToFront();
+        }
+
+        private void productToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void unitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (unit.IsDisposed)
+                unit = new frmUnit();
+            unit.MdiParent = this;
+            unit.Show();
+            unit.BringToFront();
+        }
+
+        private void employeeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (employee.IsDisposed)
+                employee = new frmEmployee();
+            employee.MdiParent = this;
+            employee.Show();
+            employee.BringToFront();
+        }
+
+        private void purchaseDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ POS System/frmtest.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace BDJBatch18.POS
+{
+    partial class frmtest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.myGrid1 = new BDJBatch18.POS.Presentation.MyControls.MyGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.myGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myGrid1
+            // 
+            this.myGrid1.AllowUserToAddRows = false;
+            this.myGrid1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.MistyRose;
+            this.myGrid1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.myGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myGrid1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.myGrid1.BackgroundColor = System.Drawing.Color.White;
+            this.myGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myGrid1.Location = new System.Drawing.Point(3, 12);
+            this.myGrid1.Name = "myGrid1";
+            this.myGrid1.ReadOnly = true;
+            this.myGrid1.RowTemplate.Height = 28;
+            this.myGrid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.myGrid1.Size = new System.Drawing.Size(527, 265);
+            this.myGrid1.TabIndex = 3;
+            // 
+            // frmtest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 377);
+            this.Controls.Add(this.myGrid1);
+            this.Name = "frmtest";
+            this.Text = "frmtest";
+            ((System.ComponentModel.ISupportInitialize)(this.myGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Presentation.MyControls.MyGrid myGrid1;
+    }
+}
